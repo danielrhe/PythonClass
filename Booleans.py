6906e496616fcf81793960191dc83f72b21679d4
Daniel <danielrhe@gmail.com>
2015-09-09 16:51:17 -0700

    User friendly output, Booleans.py
    Make output of true/false include original statement.

--- Booleans.py
+++ Booleans.py
@@ -1,21 +1,50 @@
 boolean = True
 print (boolean)
 #--------------------#
+print "413 == 413"
 print (413 == 413)
+
+print "\"apples\" == \"apples\"" // Note the use of escapes.
 print ("apples" == "apples")
+
+print "1 == 2"
 print (1 == 2)
+
+print "\"apples\" == \"gorram oranges\""
 print ("apples" == "gorram oranges")
 #--------------------#
+print "1 == 2"
 print (1 == 2)
+
+print "1 != 2"
 print (1 != 2)
+
+print "1 < 2"
 print (1 < 2)
+
+print "1 > 2"
 print (1 > 2)
+
+print "1 <= 2"
 print (1 <= 2)
+
+print "1 >= 2"
 print (1 >= 2)
 #--------------------#
+print "1 == 1"
 print (1 == 1)
+
+print "1 != 1"
 print (1 != 1)
+
+print "1 < 1"
 print (1 < 1)
+
+print "1 > 1"
 print (1 > 1)
+
+print "1 <= 1"
 print (1 <= 1)
+
+print "1 >= 1"
 print (1 >= 1)
